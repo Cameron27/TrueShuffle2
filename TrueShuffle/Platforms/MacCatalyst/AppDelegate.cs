316040cb7fbcdadd4beb0aa7dd9cc395bea10f9e
cam27 <cam27@CAMERON-PC>
2023-08-05 14:24:07 +1200

    Add project files.

---
+++ TrueShuffle/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TrueShuffle;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
